c5c2031daaec81788fa3c26936a3a24f052ff10c
jzohdi <jzohdi@terpmail.umd.edu>
2018-11-11 21:51:33 -0500

    styling/addres,phone link

--- static/scripts.js
+++ static/scripts.js
@@ -1,14 +1,32 @@
 window.onload = function() {
-  var $input = $("#datepicker").pickadate();
-  var picker = $input.pickadate("picker");
-  let minDate = new Date();
-  picker.set("min", [
-    minDate.getFullYear(),
-    minDate.getMonth(),
-    minDate.getDate() + 2
-  ]);
+  let ele = document.getElementById("datepicker");
+  if (ele) {
+    var $input = $("#datepicker").pickadate();
+    var picker = $input.pickadate("picker");
+    let minDate = new Date();
+    picker.set("min", [
+      minDate.getFullYear(),
+      minDate.getMonth(),
+      minDate.getDate() + 2
+    ]);
+    console.log("picker set");
+  }
   //   var today = new Date();
   //   var dd = today.getDate();
   //   var mm = today.getMonth();
   //   var yyyy = today.getFullYear();
 };
+
+/*
+copy text to clipBoard
+*/
+function copyToClipBoard(textValue) {
+  navigator.clipboard.writeText(textValue).then(
+    function() {
+      alert("Copied to clipboard");
+    },
+    function() {
+      console.log("failed to copy");
+    }
+  );
+}
