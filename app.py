9fe7e080698d88ba2fe9298e89a0f2ebaf6b25f0
Jacob Zohdi <jzohdi@terpmail.umd.edu>
2019-04-02 03:00:04 +0000

    reloaded static/ trying dated url for

--- app.py
+++ app.py
@@ -653,19 +653,19 @@ sess.init_app(app)
     to append ?q= + dateTime hash.
     This allows for cache busting of static files
  """
-"""
+
 @app.context_processor
 def override_url_for():
     return dict(url_for=dated_url_for)
 
 def dated_url_for(endpoint, **values):
     if endpoint == 'static':
-        filename = values.get('filename', None)
-        if filename:
-            file_path = os.path.join(endpoint, filename)
-            values['q'] = int(os.stat(file_path).st_mtime)
+#        filename = values.get('filename', None)
+#        if filename:
+#            file_path = os.path.join(endpoint, filename)
+        values['q'] = int(os.stat(file_path).st_mtime)
     return url_for(endpoint, **values)
-"""
+
 """"""
 @app.route('/')
 def index():
