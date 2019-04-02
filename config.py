e3a9419950d176714b471ad4eed27e85907b55b2
Jacob Zohdi <jzohdi@terpmail.umd.edu>
2019-04-02 01:43:29 +0000

    changed getKeys

--- config.py
+++ config.py
@@ -7,15 +7,15 @@ Created on Thu Nov  8 16:16:09 2018
 def getKeys():
     try:
         dotenv = '.env.ini'
+
+        with open(dotenv, 'r') as file:
+            content = file.readlines()
+
+        content = [line.strip().split('=') for line in content if '=' in line]
+        env_vars = dict(content)
+        if file:
+            file.close()
+        return env_vars
     except:
-        print('env file not found' )
-        
-    with open(dotenv, 'r') as file:
-        content = file.readlines()
-    
-    content = [line.strip().split('=') for line in content if '=' in line]
-    env_vars = dict(content)
-    if file:
-        file.close()
-    return env_vars
+        return False
 #    os.environ.update({"SECRET_KEY" : })
