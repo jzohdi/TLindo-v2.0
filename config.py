497dfc6d0b343a8b39b414795d36f6fe31346040
jzohdi <jzohdi@terpmail.umd.edu>
2019-08-06 23:40:18 -0400

    code refactoring, finished commit_edit_order

--- config.py
+++ config.py
@@ -15,7 +15,11 @@ def getKeys(os):
         environment = [
             'BETA_KEY', 'DATABASE_URL', 'DB',
             'EMAIL_ADDRESS', 'HOST', 'PASSWORD',
-            'PORT', 'PW', 'SHUTDOWN', 'STRIPE_KEY', 'USER', 'SECRET_KEY']
+            'PORT', 'PW', 'SHUTDOWN', 'STRIPE_KEY', 'USER',
+            'SECRET_KEY', "DB_NAME", 'USERS_DB', 'ORDERS_DB', ,
+            'PASSWORD_RECOVERY_DB', 'MENU_DB', 'SETTINGS_DB',
+            'MANAGE_DATES_DB', 'ORDER_VERSIONS', 'ERROR_DB', 'MONGO_STRING',
+            'MONGO_USER', 'MONGO_USER_PW', 'AUTH', 'EMAIL_SERVER_BASE_URI']
         for variable in environment:
             new_obj[variable] = os.environ.get(variable)
         return new_obj
