f22f935c580e4d476cafcc1dcac1d69f3ddb14cb
jzohdi <jzohdi@terpmail.umd.edu>
2019-08-06 23:41:40 -0400

    code refactoring, finished commit_edit_order

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ def getKeys(os):
             'BETA_KEY', 'DATABASE_URL', 'DB',
             'EMAIL_ADDRESS', 'HOST', 'PASSWORD',
             'PORT', 'PW', 'SHUTDOWN', 'STRIPE_KEY', 'USER',
-            'SECRET_KEY', "DB_NAME", 'USERS_DB', 'ORDERS_DB', ,
+            'SECRET_KEY', "DB_NAME", 'USERS_DB', 'ORDERS_DB',
             'PASSWORD_RECOVERY_DB', 'MENU_DB', 'SETTINGS_DB',
             'MANAGE_DATES_DB', 'ORDER_VERSIONS', 'ERROR_DB', 'MONGO_STRING',
             'MONGO_USER', 'MONGO_USER_PW', 'AUTH', 'EMAIL_SERVER_BASE_URI']
